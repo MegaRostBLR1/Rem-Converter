0c1926902e581e89f55b9d86822148f3aa2fb4b4
Rostislav <rost.tm@gmail.com>
2025-10-12 22:12:05 +0300

    init: init project

---
+++ scripts/script.js
@@ -0,0 +1,36 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const inputBase = document.querySelector('.input-base');
+    const inputRem = document.querySelector('.input-rem');
+    const inputPx = document.querySelector('.input-px');
+    const buttonClear = document.querySelector('.button-clear');
+    const error = document.querySelector('.input-error');
+
+    inputPx.addEventListener('input', (event) => {
+        if (isNaN(inputBase.value) || inputBase.value <= 0) {
+            error.style.display = 'block';
+            inputPx.value = '';
+        } else {
+            error.style.display = 'none';
+            const pxValue = event.target.value;
+            inputRem.value = (pxValue / inputBase.value).toFixed(4);
+        }
+    })
+
+    inputRem.addEventListener('input', (event) => {
+        if (isNaN(inputBase.value) || inputBase.value <= 0) {
+            error.style.display = 'block';
+            inputRem.value = '';
+        } else {
+            error.style.display = 'none';
+            const remValue = event.target.value;
+            inputPx.value = (remValue * inputBase.value).toFixed(4);
+        }
+    })
+
+    function clearInput() {
+        inputPx.value = '';
+        inputRem.value = '';
+    }
+
+    buttonClear.addEventListener('click', clearInput);
+})
